48424777986118fd21a4be3cbba7d9c6f79f51a6
fran150 <franllopez@yahoo.com.ar>
2016-12-11 23:59:44 -0300

    Added optimization
    Copies al css files to output, scans css for fonts and copies them into output dir too.

--- css.js
+++ css.js
@@ -1,14 +1,10 @@
-define(function () {
+define(['text'], function (text) {
 
     'use strict';
 
-    if (typeof window === 'undefined') {
-        return {
-            load: function (n, r, load) {
-                load()
-            }
-        };
-    }
+    var cssOutDir;
+    var dirBaseUrl;
+    var buildMap = {};
 
     function checkIfLoaded(link, fn) {
         var engine = window.navigator.userAgent.match(/Trident\/([^ ;]*)|AppleWebKit\/([^ ;]*)|Opera\/([^ ;]*)|rv\:([^ ;]*)(.*?)Gecko\/([^ ;]*)|MSIE\s([^ ;]*)|AndroidWebKit\/([^ ;]*)/) || 0,
@@ -41,26 +37,50 @@ define(function () {
     }
 
     return {
-        load: function (name, req, load) {
-            var head = document.getElementsByTagName('head')[0],
-                dataSelector = name.replace(/\/|:|\./g, '-'),
-                link;
+        load: function (name, req, onLoad, config) {
+            if (config.isBuild && ((req.toUrl(name).indexOf('empty:') === 0) || (req.toUrl(name).indexOf('http:') === 0) || (req.toUrl(name).indexOf('https:') === 0))) {
+                //avoid inlining cache busted JSON or if inlineJSON:false
+                //and don't inline files marked as empty!
+                onLoad(null);
+            } else {
+                if (config.isBuild) {
+                    if (config.cssOutDir) {
+                        cssOutDir = config.cssOutDir;
+                    } else {
+                        throw new Error('Must specify \'cssOutDir\' config parameter');
+                    }
 
-            dataSelector = dataSelector.replace(/(\-+)/g, '-');
+                    dirBaseUrl = config.dirBaseUrl;
 
-            if (!document.querySelector('[data-css-loaded=' + dataSelector + ']')) {
-                link = document.createElement('link');
+                    var file = req.toUrl(name);
+                    file = file.replace(config.dirBaseUrl, '') + '.css';
 
-                link.setAttribute('href', req.toUrl(name) + '.css');
-                link.setAttribute('rel', 'stylesheet');
-                link.setAttribute('type', 'text/css');
+                    buildMap[name] = file;
 
-                checkIfLoaded(link, function () {
-                    link.setAttribute('data-css-loaded', dataSelector);
-                    load();
-                });
+                    onLoad();
+                } else {
 
-                head.appendChild(link);
+                    var head = document.getElementsByTagName('head')[0],
+                        dataSelector = name.replace(/\/|:|\./g, '-'),
+                        link;
+
+                    dataSelector = dataSelector.replace(/(\-+)/g, '-');
+
+                    if (!document.querySelector('[data-css-loaded=' + dataSelector + ']')) {
+                        link = document.createElement('link');
+
+                        link.setAttribute('href', req.toUrl(name) + '.css');
+                        link.setAttribute('rel', 'stylesheet');
+                        link.setAttribute('type', 'text/css');
+
+                        checkIfLoaded(link, function () {
+                            link.setAttribute('data-css-loaded', dataSelector);
+                            onLoad();
+                        });
+
+                        head.appendChild(link);
+                    }
+                }
             }
         },
         normalize: function (name, normalize) {
@@ -68,6 +88,60 @@ define(function () {
                 name = name.substr(0, name.length - 4);
             }
             return normalize(name);
+        },
+        write: function(pluginName, moduleName, write) {
+            if (moduleName in buildMap) {
+                var source = path.resolve(dirBaseUrl, buildMap[moduleName]);
+                var target = path.resolve(cssOutDir, buildMap[moduleName]);
+
+                var fs = require.nodeRequire('fs-extra');
+                var css = require.nodeRequire('css');
+
+                text.get(source, function(data) {
+                    var dirSource = path.dirname(source);
+                    var dirTarget = path.dirname(target);
+
+                    var ast = css.parse(data);
+
+                    for (var i = 0; i < ast.stylesheet.rules.length; i++) {
+                        var rule = ast.stylesheet.rules[i];
+
+                        if (rule.type == 'font-face') {
+                            for (var j = 0; j < rule.declarations.length; j++) {
+                                var declaration = rule.declarations[j];
+
+                                if (declaration.property == 'src') {
+                                    var matches = declaration.value.match(/url\((.*?)\)/g);
+
+                                    for (var k = 0; k < matches.length; k++) {
+                                        var url = matches[k];
+
+                                        var from = url.indexOf("'");
+                                        var to = url.lastIndexOf("'");
+
+                                        url = url.substring(from + 1, to);
+
+                                        var options = url.indexOf("?");
+
+                                        if (options != -1) {
+                                            url = url.substring(0, options);
+                                        }
+
+                                        var fontSource = path.resolve(dirSource, url);
+                                        var fontTarget = path.resolve(dirTarget, url);
+
+                                        console.log(fontSource, '->', fontTarget);
+
+                                        fs.copySync(fontSource, fontTarget);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    fs.copySync(source, target);
+                });
+            }
         }
     }
 });
