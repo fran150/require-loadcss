4f8fc7d1b1ce33c6632c77c5aa8df9b6114611f8
admin <fllopez@bpba.com.ar>
2018-01-09 18:09:31 -0300

    - Added file generation on LayerEnd - Better bundle processing - Load repeated bundles only first time - Css minification - Added exclude option - Added option to wait for css to call onLoad or call it inmediately after link tag insertion - Added error handling when processing fonts - Changes font location to local server

--- css.js
+++ css.js
@@ -2,12 +2,15 @@ define(['text'], function (text) {
     
     'use strict';
 
+    var uglify;
     var cssOutDir;
     var dirBaseUrl;
     var buildMap = {};
     var bundlesMap = {};
     var cssConfig;
     var cssLocation = {};
+    var generatedFiles = {};
+
 
     function checkIfLoaded(link, fn) {
         var engine = window.navigator.userAgent.match(/Trident\/([^ ;]*)|AppleWebKit\/([^ ;]*)|Opera\/([^ ;]*)|rv\:([^ ;]*)(.*?)Gecko\/([^ ;]*)|MSIE\s([^ ;]*)|AndroidWebKit\/([^ ;]*)/) || 0,
@@ -65,11 +68,19 @@ define(['text'], function (text) {
     }
 
     function getBundleFileName(bundleName, cssConfig, cssOutDir) {
-        if (cssConfig && cssConfig.bundles) {
-            return path.resolve(cssOutDir, bundleName + ".css");
+        return path.resolve(cssOutDir, bundleName + ".css");
+    }
+
+    function isExcluded(moduleName, cssConfig) {
+        if (cssConfig && cssConfig.exclude) {
+            for (var i = 0; i < cssConfig.exclude.length; i++) {
+                if (cssConfig.exclude[i] == moduleName) {
+                    return true;
+                }
+            }
         }
 
-        return "";
+        return false;
     }
 
     return {
@@ -82,28 +93,34 @@ define(['text'], function (text) {
                 if (config.isBuild) {
                     cssConfig = config.css;
 
-                    // Get the cssOutDir
-                    if (config.cssOutDir) {
-                        cssOutDir = config.cssOutDir;
-                    } else {
-                        throw new Error('Must specify \'cssOutDir\' config parameter');
+                    if (!isExcluded(name, cssConfig)) {
+                        if (config.css) {
+                            uglify = config.css.uglify;
+                        }
+
+                        // Get the cssOutDir
+                        if (config.cssOutDir) {
+                            cssOutDir = config.cssOutDir;
+                        } else {
+                            throw new Error('Must specify \'cssOutDir\' config parameter');
+                        }
+
+                        // Get the dir base url
+                        dirBaseUrl = config.dirBaseUrl;
+
+                        // Get the file source
+                        var source = req.toUrl(name) + ".css";
+
+                        // Get the corresponding bundle if any
+                        var bundle = findBundle(req, cssConfig, source);
+
+                        buildMap[name] = {
+                            file: source,
+                            bundle: bundle
+                        }
+
+                        bundlesMap[name] = bundle;
                     }
-
-                    // Get the dir base url
-                    dirBaseUrl = config.dirBaseUrl;
-
-                    // Get the file source
-                    var source = req.toUrl(name) + ".css";
-                    
-                    // Get the corresponding bundle if any
-                    var bundle = findBundle(req, cssConfig, source);
-
-                    buildMap[name] = {
-                        file: source,
-                        bundle: bundle
-                    }
-
-                    bundlesMap[name] = bundle;
 
                     onLoad();
                 } else {
@@ -111,16 +128,23 @@ define(['text'], function (text) {
                     var url;
                     var nonStripName;
 
-                    debugger;
+                    if (config.cssOutDir) {
+                        cssOutDir = config.cssOutDir;
+                    }
 
                     // If theres a cssConfig entry on require (added by compiler)
-                    if (config.cssConfig) {
+                    if (config.css) {
                         // And theres a bundle config for the current module
-                        if (config.cssConfig[name]) {
-                            var bundle = config.cssConfig[name];
+                        if (config.css[name]) {
+                            var bundle = config.css[name];
                             var parsed = text.parseName(bundle);
                             nonStripName = parsed.moduleName + (parsed.ext ? '.' + parsed.ext : '');
-                            url = req.toUrl(nonStripName) + ".css";                            
+                            
+                            if (cssOutDir) {
+                                url = req.toUrl(cssOutDir + "/" + nonStripName) + ".css";
+                            } else {
+                                url = req.toUrl(nonStripName) + ".css";
+                            }                            
                         }
                     }
 
@@ -137,19 +161,30 @@ define(['text'], function (text) {
 
                     dataSelector = dataSelector.replace(/(\-+)/g, '-');
 
-                    if (!document.querySelector('[data-css-loaded=' + dataSelector + ']')) {
+                    if (!document.querySelector('[data-css-set=' + dataSelector + ']')) {
                         link = document.createElement('link');
 
                         link.setAttribute('href', url);
                         link.setAttribute('rel', 'stylesheet');
                         link.setAttribute('type', 'text/css');
+                        link.setAttribute('data-css-set', dataSelector);
 
-                        checkIfLoaded(link, function () {
+                        head.appendChild(link);                        
+                        
+                        if (config.cssConfig && config.cssConfig.waitFileLoad) {
+                            console.log("waiting");
+                            checkIfLoaded(link, function () {
+                                console.log("done");
+                                link.setAttribute('data-css-loaded', dataSelector);
+                                onLoad();
+                            });                            
+                        } else {
+                            console.log("direct");
                             link.setAttribute('data-css-loaded', dataSelector);
                             onLoad();
-                        });
-
-                        head.appendChild(link);
+                        }
+                    } else {
+                        onLoad();
                     }
                 }
             }
@@ -161,9 +196,34 @@ define(['text'], function (text) {
             return normalize(name);
         },
         onLayerEnd: function (write, data) {
-            var util = require.nodeRequire('util');
+            var util = require.nodeRequire('util');            
 
-            write("require.config({ cssConfig: " +  util.inspect(bundlesMap) + " })");
+            write("require.config({ css: " +  util.inspect(bundlesMap) + " })");
+            write("require.config({ cssOutDir: '" + path.relative("./dist", cssOutDir) + "' })");
+
+            var fs = require.nodeRequire('fs-extra');
+
+            for (var file in generatedFiles) {
+                var content = generatedFiles[file];
+
+                if (uglify) {
+                    var options = {};
+
+                    if (cssConfig && cssConfig.cleanCss) {
+                        options = cssConfig.cleanCss;
+                    }
+                    var CleanCSS = require.nodeRequire('clean-css');
+                    var output = new CleanCSS(options).minify(content);
+
+                    fs.writeFileSync(file, output.styles);
+
+                    if (output.sourceMap) {
+                        fs.writeFileSync(file + ".map", output.sourceMap);
+                    }
+                } else {
+                    fs.writeFileSync(file, content);
+                }
+            }
         },
         write: function(pluginName, moduleName, write) {
             // If the module name is in the buildmap
@@ -176,51 +236,76 @@ define(['text'], function (text) {
 
                 var fs = require.nodeRequire('fs-extra');
                 var css = require.nodeRequire('css');
-                
-                text.get(source, function(data) {
-                    fs.appendFileSync(target, data);
 
+                text.get(source, function(data) {                    
                     var dirSource = path.dirname(source);
                     var dirTarget = path.dirname(target);
 
-                    var ast = css.parse(data);
+                    var fontDirTarget = path.resolve(dirTarget, "./font");
+                    
+                    try {
+                        var ast = css.parse(data);
+                        
+                        for (var i = 0; i < ast.stylesheet.rules.length; i++) {
+                            var rule = ast.stylesheet.rules[i];
 
-                    for (var i = 0; i < ast.stylesheet.rules.length; i++) {
-                        var rule = ast.stylesheet.rules[i];
+                            if (rule.type == 'font-face') {
+                                for (var j = 0; j < rule.declarations.length; j++) {
+                                    var declaration = rule.declarations[j];
 
-                        if (rule.type == 'font-face') {
-                            for (var j = 0; j < rule.declarations.length; j++) {
-                                var declaration = rule.declarations[j];
+                                    if (declaration.property == 'src') {
+                                        var matches = declaration.value.match(/url\((.*?)\)/g);
 
-                                if (declaration.property == 'src') {
-                                    var matches = declaration.value.match(/url\((.*?)\)/g);
+                                        for (var k = 0; k < matches.length; k++) {
+                                            var url = matches[k];
 
-                                    for (var k = 0; k < matches.length; k++) {
-                                        var url = matches[k];
+                                            url = url.replace("\'", "");
+                                            url = url.replace("\"", "");
 
-                                        url = url.replace("\'", "");
-                                        url = url.replace("\"", "");
+                                            var from = url.indexOf("(");
+                                            var to = url.lastIndexOf(")");
 
-                                        var from = url.indexOf("(");
-                                        var to = url.lastIndexOf(")");
+                                            url = url.substring(from + 1, to);
 
-                                        url = url.substring(from + 1, to);
+                                            var options = url.indexOf("?");
 
-                                        var options = url.indexOf("?");
+                                            if (options != -1) {
+                                                url = url.substring(0, options);
+                                            }
 
-                                        if (options != -1) {
-                                            url = url.substring(0, options);
+                                            var hash = url.indexOf("#");
+
+                                            if (hash != -1) {
+                                                url = url.substring(0, hash);
+                                            }
+
+                                            var fileInfo = path.parse(url);
+                                            
+                                            var fontSource = path.resolve(dirSource, url);
+                                            var fontTarget = path.resolve(fontDirTarget, fileInfo.base);
+                                            
+                                            var urlTarget = path.relative(dirTarget, fontTarget);
+                                            urlTarget = urlTarget.split("\\").join("/");                                            
+
+                                            declaration.value = declaration.value.split(url).join(urlTarget);
+
+                                            fs.copySync(fontSource, fontTarget);
                                         }
-
-                                        var fontSource = path.resolve(dirSource, url);
-                                        var fontTarget = path.resolve(dirTarget, url);
-
-                                        fs.copySync(fontSource, fontTarget);
                                     }
                                 }
                             }
                         }
+                    } catch (ex) {
+                        console.log(ex);
                     }
+
+                    var modified = css.stringify(ast);
+                    
+                    if (target in generatedFiles) {
+                        generatedFiles[target] += modified;
+                    } else {
+                        generatedFiles[target] = modified;
+                    }                    
                 });
             }
         }
